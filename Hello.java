59c4a7f942ae8c28ea604495cd70230887383527
CumiTerbang <gitarringsek@gmail.com>
2013-08-02 13:22:27 +0700

    gw ulang ngepush Hello.java ama ada yg dibenerin tadi salah ngetik

--- Hello.java
+++ Hello.java
@@ -1,5 +1,5 @@
 public class Hello{
 	public static void main(String[] args){
-		System.out.println("halo mo nyaba java push ke git");
+		System.out.println("halo mo nyoba java push ke git");
 	}
 }

088d74999fb7f96e4116ca28c606b2371acc5b4a
CumiTerbang <gitarringsek@gmail.com>
2013-08-02 15:56:09 +0700

    coBA "git commit -a"

--- Hello.java
+++ Hello.java
@@ -1,5 +1,5 @@
 public class Hello{
 	public static void main(String[] args){
-		System.out.println("halo mo nyoba java push ke git");
+		System.out.println("halo mo nyoba java push ke git, ada yg gw edit");
 	}
 }
